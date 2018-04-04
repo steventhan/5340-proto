95e2219d9863013bdf9b47728a739aacf72c0859
Steven Than <steventhan11@gmail.com>
2018-04-04 04:26:07 -0400

    cleaned up unused localstage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-localStorage.setItem("reservations", JSON.stringify([]));
 localStorage.setItem("notifications", JSON.stringify([]));
 
 ReactDOM.render(<App />, document.getElementById('root'));
